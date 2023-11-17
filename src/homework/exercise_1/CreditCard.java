8dfd5c03ceedbb6a5becb7653e1a543e90f19826
ddumaa <dimaduma62@gmail.com>
2023-11-17 22:48:21 +0300

    update 1.1

--- src/homework/exercise_1/CreditCard.java
+++ src/homework/exercise_1/CreditCard.java
@@ -3,19 +3,17 @@ package homework.exercise_1;
 public class CreditCard {
     int accountNumber;
     double amountAccount;
-    public double addAmount (double summa){
+    public void addAmount (double summa){
         amountAccount += summa;
         System.out.println("The balance is replenished by the amount: " + summa);
-        return amountAccount;
     }
-    public double withdraw (double summa){
+    public void withdraw (double summa){
         if(summa > amountAccount){
             System.out.println("Insufficient funds on balance");
         }else{
             amountAccount -= summa;
             System.out.println("Amount withdrawn");
         }
-        return amountAccount;
     }
     public void balanceInfo (){
         System.out.println("Your balance: " + amountAccount);

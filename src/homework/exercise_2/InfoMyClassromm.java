abb3122d8163e178e4ae635720560216605d277f
ddumaa <dimaduma62@gmail.com>
2023-11-17 23:27:36 +0300

    completed task 2

---
+++ src/homework/exercise_2/InfoMyClassromm.java
@@ -0,0 +1,19 @@
+package homework.exercise_2;
+
+public class InfoMyClassromm {
+    public static void main(String[] args) {
+        MyClassroom person1 = new MyClassroom("Andrey", "Divin", "AB5657987BB7", "c26");
+        MyClassroom person2 = new MyClassroom("Olya", "Neronena", "AB4386557BB1", "c26");
+        MyClassroom person3 = new MyClassroom("Vyasya", "Kolivik", "AB6557977BB4", "c26");
+        MyClassroom person4 = new MyClassroom("Anton", "Terin", "AB1257946BB8", "c26");
+
+        String[][] groupBase= {{person1.firstname, person1.lastname, person1.passportID, person1.groupName},{person2.firstname, person2.lastname, person2.passportID, person2.groupName},{person3.firstname, person3.lastname, person3.passportID, person3.groupName},{person4.firstname, person4.lastname, person4.passportID, person4.groupName}};
+
+        for (String[] p1: groupBase) {
+            System.out.println(" ");
+            for (String p2: p1){
+                System.out.print(p2 + " ");
+            }
+        }
+    }
+}
---
+++ src/homework/exercise_2/MyClassroom.java
@@ -0,0 +1,15 @@
+package homework.exercise_2;
+
+public class MyClassroom {
+    String firstname;
+    String lastname;
+    String passportID;
+    String groupName;
+
+    public MyClassroom (String firstname1, String lastname1, String passportID1, String groupName1){
+        firstname = firstname1;
+        lastname = lastname1;
+        passportID = passportID1;
+        groupName = groupName1;
+    }
+}

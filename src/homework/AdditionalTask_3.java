13a2bd4873baed8fc2dd20fa1756f38b12b4303b
ddumaa <dimaduma62@gmail.com>
2023-11-19 13:28:51 +0300

    completed task 3

---
+++ src/homework/AdditionalTask_3.java
@@ -0,0 +1,22 @@
+package homework;
+
+import java.util.Scanner;
+
+public class AdditionalTask_3 {
+    public static double bank(double a, int years){
+        for (int i = 0; i <= years; i++) {
+            a += a * 10.0/100;
+        }
+        System.out.println("Your contribution for " + years + " years, will be: " + a);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("how much do you want to deposit?");
+        double a = scanner.nextDouble();
+        System.out.println("For how long?");
+        int years = scanner.nextInt();
+        double myDeposit = bank(a, years);
+    }
+}

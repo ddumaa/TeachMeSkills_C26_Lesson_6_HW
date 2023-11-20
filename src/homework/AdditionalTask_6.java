bf0558f95d6e17c5f6ea457fdeda04ac85a56a5e
ddumaa <dimaduma62@gmail.com>
2023-11-20 11:52:27 +0300

    update program

--- src/homework/AdditionalTask_6.java
+++ src/homework/AdditionalTask_6.java
@@ -10,11 +10,11 @@ package homework;
  */
 public class AdditionalTask_6 {
     public static void main(String[] args) {
-        int[] arrays = {2, 4, -6, 7, -1, 8, 3, 10, -9, 1};
+        int[] arrays = {-6, 2, 4, 7, -1, 0, -10, 8, 3, 10, -9, 1};
         for (int i = 0; i < arrays.length; i++) {
-            for (int j = i; j < arrays.length; j++) {
-                for (int k = j; k < arrays.length; k++) {
-                    if(arrays[i] + arrays[j] + arrays[k] == 0 && i!=j && j!=k) {
+            for (int j = 0; j < arrays.length; j++) {
+                for (int k = 0; k < arrays.length; k++) {
+                    if(arrays[i] + arrays[j] + arrays[k] == 0 && arrays[i] <= arrays[j] && arrays[j] <= arrays[k] && i!=j && j!=k) {
                         System.out.println(arrays[i] + " + " + arrays[j] + " + " + arrays[k] + " = 0");
                     }else if (i == arrays.length-1) {
                         System.out.println("there are no more combinations");
